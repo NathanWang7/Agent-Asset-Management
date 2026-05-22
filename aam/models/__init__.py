4fd4ce7645cdd9fab4b0afcc1500cffaf56437d8
Nathan <472688661@qq.com>
2026-05-22 13:44:09 +0800

    feat: add core manifest models

--- aam/models/__init__.py
+++ aam/models/__init__.py
@@ -1,1 +1,23 @@
 """Typed domain models package for Agent Asset Management."""
+
+from aam.models.manifest import (
+    AssetCardManifest,
+    AssetManifest,
+    ContextCostSpec,
+    PackageInfo,
+    PackageManifest,
+    PermissionSpec,
+    ProfileManifest,
+    SourceProvenance,
+)
+
+__all__ = [
+    "AssetCardManifest",
+    "AssetManifest",
+    "ContextCostSpec",
+    "PackageInfo",
+    "PackageManifest",
+    "PermissionSpec",
+    "ProfileManifest",
+    "SourceProvenance",
+]
